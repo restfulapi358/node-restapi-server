a6e29a0b3ce0b93ed5b53404347ac718eee0894f
restfulapi358 <restfulapi358@gmail.com>
2024-06-19 23:23:55 -0700

    add hello and shutdown

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const port = 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// /hello route
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+// /shutdown route
+app.post('/shutdown', (req, res) => {
+  res.json({ message: 'Server shutting down...' });
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
+
 // Basic route
 app.get('/v1/api', (req, res) => {
   res.json({message: 'Welcome to my RESTful API'});
